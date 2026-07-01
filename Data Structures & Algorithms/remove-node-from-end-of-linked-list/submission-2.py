bd3877d0ec6951634a83445350d3f83e59f05c25
Grace Cimaszewski <gcimaszewski@gmail.com>
2026-07-01 12:43:40 -0700

    Add: remove-node-from-end-of-linked-list - submission-2

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-2.py
@@ -0,0 +1,57 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    """
+    Given the head of a linked list and an integer n,
+    remove the nth node from the end of the list and return its head.
+    """
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+
+        # fast-slow pointer method
+        # advance the fast pointer n places,
+        # then advance both until fast at None
+
+        # [1, 2, 3]
+        # fast= 3; slow=1
+        # fast= None; slow=2
+
+        # fast= None; slow=3
+
+
+        #fast, slow = head, head
+        fast = head
+        for i in range(n):
+            fast = fast.next
+        dummy = ListNode(val=-1, next=head)
+        left = dummy
+        before_slow = None
+        while fast is not None:
+           # before_slow = slow
+           # slow = slow.next
+            left = left.next
+            fast = fast.next
+        # now `slow` is pointing to nth from end node
+        # to delete it: set its predecessor node to slow.next
+        # case where the head node is the one deleted: 
+        # if not before_slow:
+        #     return slow.next
+        # before_slow.next = slow.next
+        # return head
+        # normal case
+        left.next = left.next.next
+        return dummy.next
+        # if not dummy.next:
+        #     return None
+        # if dummy_at_head:
+        #     return head.next
+        
+
+        # dummy.next = dummy.next.next
+        # return head
+        
+
+        
