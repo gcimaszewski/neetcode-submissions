ae8ba04e3087ad2a200a4af5e59b8c66f3ae676b
Grace Cimaszewski <gcimaszewski@gmail.com>
2026-07-05 22:39:04 -0700

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.py
@@ -0,0 +1,39 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        # this is a little similar to combination generation
+        # each place in digits is a level in the decision tree
+        # each path corresponds to one letter that digit maps to
+
+        if len(digits) == 0:
+            return []
+
+        combs = []
+        state = []
+
+        digit_char_mapping = {
+            2: ['a', 'b', 'c'],
+            3: ['d', 'e', 'f'],
+            4: ['g', 'h', 'i'],
+            5: ['j', 'k', 'l'],
+            6: ['m', 'n', 'o'],
+            7: ['p', 'q', 'r', 's'],
+            8: ['t', 'u', 'v'],
+            9: ['w', 'x', 'y', 'z']
+        }
+
+        def backtrack(i):
+            if i >= len(digits):
+                combs.append("".join(state))
+                return
+            
+            for char in digit_char_mapping[int(digits[i])]:
+                state.append(char)
+                backtrack(i+1)
+                state.pop()
+        
+        backtrack(0)
+        return combs
+            # for each choice (e.g., possible letter mapping):
+            #     add that choice to the current running mapping
+            #     backtrack
+            #     undo the choice to continue onto the next one
