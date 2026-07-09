664230d213cbd0ee2472cb6628fdac52cf8d801d
Grace Cimaszewski <gcimaszewski@gmail.com>
2026-07-08 22:22:11 -0400

    Add: clone-graph - submission-9

---
+++ Data Structures & Algorithms/clone-graph/submission-9.py
@@ -0,0 +1,38 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    """
+    Clone Graph
+
+    Given a node in a connected undirected graph, return a deep copy of the graph.
+
+    Each node in the graph contains an integer value and a list of its neighbors.
+
+    """
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return
+
+        idx_to_node = {}
+        q = [node]
+        while q:
+            curr = q.pop()
+
+            try:
+                new_node = idx_to_node[curr.val]
+            except KeyError:
+                new_node = Node(curr.val)
+                idx_to_node[curr.val] = new_node
+            for nei in curr.neighbors:
+                if nei.val not in idx_to_node:
+                    idx_to_node[nei.val] = Node(nei.val)
+                    q.append(nei)
+                idx_to_node[curr.val].neighbors.append(idx_to_node[nei.val])
+
+        return idx_to_node[1]
