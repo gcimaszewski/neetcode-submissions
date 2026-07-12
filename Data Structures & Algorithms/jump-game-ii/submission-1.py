325e9d0b08560420ec9b174def084986f2edd225
Grace Cimaszewski <gcimaszewski@gmail.com>
2026-07-12 17:04:00 -0400

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        # intuitively: we can construct the shortest path to the goal
+        # by finding the indices that allow us to hop the farthest
+        # greedy: at index i, if we can jump to index j, we can also jump to i+1, ..., j-1
+        # for each index: check if we can jump directly to the goal
+        # otherwise: compute our jumping range. 
+        # because we start from the back of the array, the number of hops for these spaces are already computed
+        # pick the next hop that has the shortest path to goal
+        # hop2goal(i) = min(hop2goal(i+1),hop2goal(i+2),...,hop2goal(j)) + 1
+
+        # idea: store the "latest" hop that has a certain distance
+        last_idx_with_dist = {}
+        
+        goal = len(nums) - 1
+        dist = {goal: 0}
+        for i in range(len(nums) - 2, -1, -1):
+            hop_range = nums[i]
+            furthest_hop = i + hop_range
+            if furthest_hop >= goal:
+                last_idx_with_dist[1] = i
+                dist[i] = 1
+            elif hop_range == 0:
+                # gulley - no-go zone
+                dist[i] = float('inf')
+            else:
+                # we have other intermediate hop(s) to make
+                best_next_dist = float('inf')
+                for h in range(i+1,furthest_hop+1):
+                    best_next_dist = min(best_next_dist, dist[h])
+                dist[i] = best_next_dist + 1
+        return dist[0]
